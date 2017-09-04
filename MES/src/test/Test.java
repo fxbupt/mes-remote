63e4d630d14636eb48283edf6bf3fea8d73d4aee
fxbupt <fxbupt@sina.com>
2017-09-04 14:56:49 +0800

    重命名视图解决冲突

--- MES/src/test/Test.java
+++ MES/src/test/Test.java
@@ -2,7 +2,7 @@ package test;
 
 import com.cims.mes.utils.UnitedSerialNumber;
 
-public class UnitedSerialNumberTest {
+public class Test {
   public static void main(String[] args) {
     UnitedSerialNumber USN = new UnitedSerialNumber();
     System.out.println(USN.generateSerialNumber());
